8dfdac435a80501e346e24fcbfb2ca6035f34968
Tayyab ur rehman <126967010+Tayyab16203@users.noreply.github.com>
2024-02-12 16:19:16 +0500

    ini news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,9 +7,9 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/10/01/1600x900/shivaji_wagh_nakh_1696152637258_1696152646858.jpg" : imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
+                <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
                 <a href={newsUrl} target='blank' className="btn btn-sm btn-primary">Read More</a>
             </div>
